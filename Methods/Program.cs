faff1aee53dab5e3ab2e1264b22e38fd5201ffc2
Atilla Güngör <atillaagungorr@gmail.com>
2023-11-09 14:32:09 +0300

    Yazilim Gelistirici Gun 3

---
+++ Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product products = new Product();
+            products.Name = "Elma";
+            products.Price = 15;
+            products.Description = "Amasya elması";
+
+            Product products2 = new Product();
+            products2.Name = "Karpuz";
+            products2.Price = 80;
+            products2.Description = "Diyarbakır karpuzu";
+
+            Product[] products3 = new Product[] { products, products2 };
+
+            //type-safe -- tip güvenli
+            foreach (Product product in products3)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Description);
+                Console.WriteLine("------------------");
+            }
+
+            Console.WriteLine("---------Methods---------");
+            //instance
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(products);
+            sepetManager.Add2("Armut", "Yeşil armut", 12,10);
+            sepetManager.Add2("Elma", "Yeşil elma", 12,9);
+            sepetManager.Add2("Karpuz", "Diyarbakır karpuzu", 12,8);
+        }
+    }
+}
---
+++ Methods/SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class SepetManager
+    {
+        //naming convention
+        public void Add(Product product)
+        {
+            Console.WriteLine("Tebrikler! Sepete Eklendi: " + product.Name);
+        }
+
+        public void Add2(string productName, string productDescription, double price, int stockQuantity)
+        {
+            Console.WriteLine("Tebrikler! Sepete Eklendi: " + productName);
+        }
+    }
+}
